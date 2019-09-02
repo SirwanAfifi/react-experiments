6c9e97e785ed230f9253862f39e6146a6aa4f92d
sirwan <sir1afifi@gmail.com>
2019-09-02 11:07:21 +0430

    Add click hander

--- todo/src/App.js
+++ todo/src/App.js
@@ -3,11 +3,31 @@ import React, { Component } from 'react';
 // import './App.css';
 
 export default class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "Sirwan"
+    }
+  }
+
+  changeStateData = () => {
+    this.setState({
+      username: this.state.username === "Sirwan" ? "Kaywan" : "Sirwan"
+    })
+  }
+
   render() {
     return (
-      <h4 className="bg-primary text-white text-center p-2">
-        To Do List
-      </h4>
+      <div>
+        <h4 className="bg-primary text-white text-center p-2">
+          {this.state.username}'s To Do List
+        </h4>
+        <button className="btn btn-primary m-2"
+          onClick={this.changeStateData}>
+          Change
+                </button>
+      </div>
     );
   }
 }
