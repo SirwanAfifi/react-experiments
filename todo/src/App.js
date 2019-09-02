21947f831e46405e36d51a03e2ad0e901f8c9843
sirwan <sir1afifi@gmail.com>
2019-09-02 11:10:37 +0430

    use fat arrow function for render method

--- todo/src/App.js
+++ todo/src/App.js
@@ -17,17 +17,13 @@ export default class App extends Component {
     })
   }
 
-  render() {
-    return (
-      <div>
-        <h4 className="bg-primary text-white text-center p-2">
-          {this.state.username}'s To Do List
-        </h4>
-        <button className="btn btn-primary m-2"
-          onClick={this.changeStateData}>
-          Change
-                </button>
-      </div>
-    );
-  }
+  render = () => <div>
+    <h4 className="bg-primary text-white text-center p-2">
+      {this.state.username}'s To Do List
+  </h4>
+    <button className="btn btn-primary m-2"
+      onClick={this.changeStateData}>
+      Change
+          </button>
+  </div>
 }
