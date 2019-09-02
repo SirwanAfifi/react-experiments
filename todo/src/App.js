269bbdf2918b5def574a2066561f9fbebdb4c1c0
sirwan <sir1afifi@gmail.com>
2019-09-02 10:45:12 +0430

    add todo

---
+++ todo/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+
+export default class App extends Component {
+  render() {
+    return (
+      <h1 className="bg-primary text-white text-center p-2">
+        To Do List
+      </h1>
+    );
+  }
+}
