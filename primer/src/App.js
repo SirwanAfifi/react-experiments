da94c83d33f2b5c2f85e3c8aeccf60e5143b9e59
sirwan <sir1afifi@gmail.com>
2019-09-03 13:10:22 +0430

    Use expression

--- primer/src/App.js
+++ primer/src/App.js
@@ -2,8 +2,20 @@ import React, { Component } from 'react';
 
 const message = "This is a constant";
 export default class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 4
+    };
+  }
+
+  isEven() {
+    return this.state.count % 2 === 0 ? "Even" : "Odd";
+  }
+
   render = () =>
     <h4 className="bg-primary text-white text-center p-2 m-1">
-      {message}
+      Number of things: {this.isEven(this.state.count)}
     </h4>
 }
