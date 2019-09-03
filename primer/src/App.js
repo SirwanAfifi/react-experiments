17059950dd5743eb88a0e6b5468a07c86ebe0392
sirwan <sir1afifi@gmail.com>
2019-09-03 13:07:08 +0430

    removed extra markups

--- primer/src/App.js
+++ primer/src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 
+const message = "This is a constant";
 export default class App extends Component {
-  render = () => <h4 className="bg-primary text-white text-center p-2 m-1">
-    Component Element
-</h4>
+  render = () =>
+    <h4 className="bg-primary text-white text-center p-2 m-1">
+      {message}
+    </h4>
 }
