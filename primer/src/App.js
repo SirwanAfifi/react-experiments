7f868ece181a535ce755d873fc1e9899800c5b39
sirwan <sir1afifi@gmail.com>
2019-09-03 13:12:15 +0430

    Set class name based on expression

--- primer/src/App.js
+++ primer/src/App.js
@@ -14,8 +14,14 @@ export default class App extends Component {
     return this.state.count % 2 === 0 ? "Even" : "Odd";
   }
 
+  getClassName(val) {
+    return val % 2 === 0
+      ? "bg-primary text-white text-center p-2 m-1"
+      : "bg-secondary text-white text-center p-2 m-1";
+  }
+
   render = () =>
-    <h4 className="bg-primary text-white text-center p-2 m-1">
+    <h4 className={this.getClassName(this.state.count)}>
       Number of things: {this.isEven(this.state.count)}
     </h4>
 }
