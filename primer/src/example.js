20839be9fc1944ba11151e2b81bb9a1db75046bd
sirwan <sir1afifi@gmail.com>
2019-09-04 13:16:47 +0430

    Function definition

---
+++ primer/src/example.js
@@ -0,0 +1,10 @@
+console.log("Hello");
+console.log("Apples");
+console.log("This is a statement");
+console.log("This is also a statement");
+
+const myFunc = function () {
+    console.log("This statement is inside the function");
+};
+console.log("This statement is outside the function");
+myFunc();
