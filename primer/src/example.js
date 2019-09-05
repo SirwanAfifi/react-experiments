59ec9fadf25966782eafccf509755e343b27fa41
sirwan <sir1afifi@gmail.com>
2019-09-05 10:12:59 +0430

    use reduce

--- primer/src/example.js
+++ primer/src/example.js
@@ -1,10 +1,11 @@
-console.log("Hello");
-console.log("Apples");
-console.log("This is a statement");
-console.log("This is also a statement");
+let products = [
+    { name: "Hat", price: 24.5, stock: 10 },
+    { name: "Kayak", price: 289.99, stock: 1 },
+    { name: "Soccer Ball", price: 10, stock: 0 },
+    { name: "Running Shoes", price: 116.50, stock: 20 }
+];
 
-const myFunc = function () {
-    console.log("This statement is inside the function");
-};
-console.log("This statement is outside the function");
-myFunc();
+
+let totalValue = products
+    .filter(item => item.stock > 0)
+    .reduce((prev, item) => prev + (item.price * item.stock), 0);
