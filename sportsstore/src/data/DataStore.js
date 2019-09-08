869a8dca7995f545fddaac5e5f4ff87d14765850
sirwan <sir1afifi@gmail.com>
2019-09-09 01:16:39 +0430

    Async action creators

--- sportsstore/src/data/DataStore.js
+++ sportsstore/src/data/DataStore.js
@@ -1,4 +1,5 @@
-import { createStore } from "redux";
-import { ShopReducer } from "./ShopReducer"; import { CartReducer } from "./CartReducer"; import { CommonReducer } from "./CommonReducer";
+import { createStore, applyMiddleware } from "redux";
+import { ShopReducer } from "./ShopReducer"; import { CartReducer } from "./CartReducer"; import { CommonReducer } from "./CommonReducer"; import { asyncActions } from "./AsyncMiddleware";
 export const SportsStoreDataStore
-    = createStore(CommonReducer(ShopReducer, CartReducer));
+    = createStore(CommonReducer(ShopReducer, CartReducer),
+        applyMiddleware(asyncActions));
