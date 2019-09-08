39397c278171643df2764c2b06884c2cd879d6b6
sirwan <sir1afifi@gmail.com>
2019-09-08 14:40:43 +0430

    add shoping cart

--- sportsstore/src/shop/ShopConnector.js
+++ sportsstore/src/shop/ShopConnector.js
@@ -5,12 +5,15 @@ import { connect } from "react-redux";
 import { loadData } from "../data/ActionCreators";
 import { DataTypes } from "../data/Types";
 import { Shop } from "./Shop";
-
+import { addToCart, updateCartQuantity, removeFromCart, clearCart }
+    from "../data/CartActionCreators";
+import { CartDetails } from "./CartDetails";
 const mapStateToProps = (dataStore) => ({
     ...dataStore
 })
 const mapDispatchToProps = {
-    loadData
+    loadData, addToCart, updateCartQuantity, removeFromCart, clearCart
+
 }
 const filterProducts = (products = [], category) =>
     (!category || category === "All")
@@ -26,6 +29,8 @@ export const ShopConnector = connect(mapStateToProps, mapDispatchToProps)(
                         <Shop {...this.props} {...routeProps}
                             products={filterProducts(this.props.products,
                                 routeProps.match.params.category)} />} />
+                <Route path="/shop/cart" render={(routeProps) =>
+                    <CartDetails {...this.props} {...routeProps} />} />
                 <Redirect to="/shop/products" />
             </Switch>
         }
