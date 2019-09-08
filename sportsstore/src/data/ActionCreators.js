8778f7fa0bd4b8e20e540babc0f825d8c8bbdffb
sirwan <sir1afifi@gmail.com>
2019-09-08 13:50:27 +0430

    Add data connector

--- sportsstore/src/data/ActionCreators.js
+++ sportsstore/src/data/ActionCreators.js
@@ -5,6 +5,6 @@ export const loadData = (dataType) => ({
     type: ActionTypes.DATA_LOAD,
     payload: {
         dataType: dataType,
-        data: data
+        data: phData[dataType]
     }
 });
