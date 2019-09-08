7e0b89cead9dcf09fb774bdada0a412919c72fa5
sirwan <sir1afifi@gmail.com>
2019-09-08 14:05:08 +0430

    Add reducers

--- sportsstore/src/data/DataStore.js
+++ sportsstore/src/data/DataStore.js
@@ -1,3 +1,4 @@
 import { createStore } from "redux";
-import { ShopReducer } from "./ShopReducer";
-export const SportsStoreDataStore = createStore(ShopReducer);
+import { ShopReducer } from "./ShopReducer"; import { CartReducer } from "./CartReducer"; import { CommonReducer } from "./CommonReducer";
+export const SportsStoreDataStore
+    = createStore(CommonReducer(ShopReducer, CartReducer));
