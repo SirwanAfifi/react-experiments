69294643eea36e41a6f6ebce5a2dff90dacda95f
sirwan <sir1afifi@gmail.com>
2019-09-09 23:49:36 +0430

    Static contents

--- projecttools/src/App.js
+++ projecttools/src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import "./App.css"
+import reactLogo from "./logo.svg";
 
 let name = "Sirwan";
 const city = "Sanandaj";
@@ -8,7 +10,10 @@ export default class App extends Component {
 	message = () => `Hello ${name} from ${city}`;
 
 	render = () =>
-		<h4 className="bg-primary text-white text-center p-3">
-			{this.message()}
-		</h4>
+		<div className="text-center">
+			<h4 className="bg-primary text-white text-center p-3">
+				{this.message()}
+			</h4>
+			<img src={reactLogo} alt="reactLogo" />
+		</div>
 }
