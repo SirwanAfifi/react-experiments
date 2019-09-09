4ed90c0aea8621cfe34411c70a877f24a0ac743c
sirwan <sir1afifi@gmail.com>
2019-09-09 10:46:47 +0430

    Add admin

--- sportsstore/src/data/Urls.js
+++ sportsstore/src/data/Urls.js
@@ -7,5 +7,7 @@ const port = 3500;
 export const RestUrls = {
     [DataTypes.PRODUCTS]: `${protocol}://${hostname}:${port}/api/products`,
     [DataTypes.CATEGORIES]: `${protocol}://${hostname}:${port}/api/categories`,
-    [DataTypes.ORDERS]: `${protocol}://${hostname}:${port}/api/orders`    
+    [DataTypes.ORDERS]: `${protocol}://${hostname}:${port}/api/orders`
 }
+
+export const GraphQlUrl = `${protocol}://${hostname}:${port}/graphql`;
