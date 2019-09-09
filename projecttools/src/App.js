f8ab0a14de9092897502238f28aa1292eeff48e5
sirwan <sir1afifi@gmail.com>
2019-09-09 13:45:35 +0430

    code transformation

--- projecttools/src/App.js
+++ projecttools/src/App.js
@@ -1,26 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+	render = () => React.createElement("h4",
+		{ className: "bg-primary text-white text-center p-3" },
+		React.createElement("span", {
+			className: "text-white"
+		}, "Hello")
+	)
 }
-
-export default App;
