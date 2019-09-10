af2eea178154e1f96007189afb6d3849a1e08f1a
sirwan <sir1afifi@gmail.com>
2019-09-10 11:15:08 +0430

    add new conditional rendering

--- components/src/Message.js
+++ components/src/Message.js
@@ -2,9 +2,11 @@ import React from 'react';
 
 export function Message(props) {
     if (props.name === "Sirwan") {
-        return <h1 className="bg-primary text-white text-center p-3">Hello Man, Ha ya doin?!</h1>
-    }
+        return <h1 className="bg-warning text-white text-center p-3">Hello Man, Ha ya doin?!</h1>
+    } else if (props.name === "Behzad") {
+        return <h1 className="bg-danger text-white text-center p-3">This is the guy!</h1>
 
+    }
     return (
         <h4 className="bg-success text-white text-center p-2">
             {props.greeting}, {props.name}
