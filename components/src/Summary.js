a47620ad045fbf747f18ff9f61e0c580e5a37465
sirwan <sir1afifi@gmail.com>
2019-09-10 11:45:32 +0430

    add validation

--- components/src/Summary.js
+++ components/src/Summary.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
 export function Summary(props) {
-    return <React.Fragment>
-        <td>{props.index + 1} </td>
-        <td>{props.name} </td>
-        <td>{props.name.length} </td>
-    </React.Fragment>
+	if (props.name.length > 4) {
+		return <>
+			<td>{props.index + 1} </td>
+			<td>{props.name} </td>
+			<td>{props.name.length} </td>
+		</>
+	}
+	return null;
 }
