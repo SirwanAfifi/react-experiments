619f9409e86267d131502b153f5d85e9c5a33497
sirwan <sir1afifi@gmail.com>
2019-09-10 11:12:51 +0430

    conditional rendering

--- components/src/Message.js
+++ components/src/Message.js
@@ -1,6 +1,10 @@
 import React from 'react';
 
 export function Message(props) {
+    if (props.name === "Sirwan") {
+        return <h1 className="bg-primary text-white text-center p-3">Hello Man, Ha ya doin?!</h1>
+    }
+
     return (
         <h4 className="bg-success text-white text-center p-2">
             {props.greeting}, {props.name}
