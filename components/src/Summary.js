1fd20e20603aa6dc2d0ea32bc938a62d23715f99
sirwan <sir1afifi@gmail.com>
2019-09-10 11:21:02 +0430

    Using map to process array

--- components/src/Summary.js
+++ components/src/Summary.js
@@ -1,21 +1,15 @@
 import React from 'react';
 
-function createInnerElements(names) {
-    let innerElems = [];
-    for (let i = 0; i < names.length; i++) {
-        innerElems.push(
-            <div>
-                {names[i]} contains {names[i].length} letters
-            </div>
-        )
-    }
-    return innerElems;
-}
-
 export function Summary(props) {
     return (
         <h4 className="bg-info text-white text-center p-2">
-            {createInnerElements(props.names)}
+            {props.names.map(name => {
+                return (
+                    <div>
+                        {name} contains {name.length} letters
+                    </div>
+                )
+            })}
         </h4>
     )
 }
