46487ab9124cac674840a41f5b6ef943cea2bacf
sirwan <sir1afifi@gmail.com>
2019-09-11 10:56:46 +0430

    Avoding dependent value pitfall

--- components/src/SimpleButton.js
+++ components/src/SimpleButton.js
@@ -1,11 +1,31 @@
 import React, { Component } from "react";
 
 export class SimpleButton extends Component {
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			counter: 0,
+			hasButtonBeenClicked: false
+		};
+	}
+
 	render = () => {
 		return (
-			<button onClick={this.props.callback}
+			<button onClick={this.handleClick}
 				className={this.props.className}>
-				{this.props.text}
+				{this.props.text} {this.state.counter}
+				{this.state.hasButtonBeenClicked &&
+					<div>Button Clicked!</div>}
 			</button>)
+	}
+
+	handleClick = () => {
+		this.setState({
+			counter: this.state.counter + 1
+		}, () => this.setState({
+			hasButtonBeenClicked: this.state.counter > 0
+		}));
+		this.props.callback();
 	}
 }
