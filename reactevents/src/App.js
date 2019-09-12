887be0ffb3255d091474b86c6e3a4456e71532b9
sirwan <sir1afifi@gmail.com>
2019-09-12 10:26:17 +0430

    update state

--- reactevents/src/App.js
+++ reactevents/src/App.js
@@ -1,21 +1,26 @@
 import React, { Component } from 'react';
 
 export default class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      message: "Ready"
-    };
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			message: "Ready"
+		};
+	}
 
-  render = () => {
-    return <div className="m-2">
-      <div className="h4 bg-primary text-white text-center p-2">
-        {this.state.message}
-      </div>
-      <div className="text-center">
-        <button className="btn btn-primary">Click</button>
-      </div>
-    </div>
-  }
+	handleEvent = () => {
+		this.setState({ message: "Clicked!" });
+	}
+
+	render = () => {
+		return <div className="m-2">
+			<div className="h4 bg-primary text-white text-center p-2">
+				{this.state.message}
+			</div>
+			<div className="text-center">
+				<button className="btn btn-primary"
+					onClick={this.handleEvent}>Click</button>
+			</div>
+		</div>
+	}
 }
