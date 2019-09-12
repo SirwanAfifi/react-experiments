49e5dc11318a59dedd426fbe0fe81dca8edc7414
sirwan <sir1afifi@gmail.com>
2019-09-12 18:31:08 +0430

    Add theme component

--- reactevents/src/App.js
+++ reactevents/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import { ThemeButton } from "./ThemeButton";
 export default class App extends Component {
 	constructor(props) {
 		super(props);
@@ -10,39 +10,27 @@ export default class App extends Component {
 		};
 	}
 
-	handleEvent = (event, newTheme) => {
-		event.persist();
+	selectTheme = (newTheme) => {
 		this.setState({
-			counter: this.state.counter + 1,
-			theme: newTheme
-		},
-			() => this.setState({ message: `${event.type}: ${this.state.counter}` }));
+			theme: newTheme,
+			message: `Theme ${newTheme}`
+		});
 	}
 
-	toggleCheckbox = (event) => {
-		if (this.state.counter === 0) {
-			event.preventDefault();
-		}
+	handleClick = (event) => {
+		console.log(`App: Type: ${event.type} `
+			+ `Target: ${event.target.tagName} `
+			+ `CurrentTarget: ${event.currentTarget.tagName}`);
 	}
-
 	render = () => {
-		return <div className="m-2">
-			<div className="form-check">
-				<input className="form-check-input"
-					type="checkbox"
-					onClick={this.toggleCheckbox} />
-				<label>This is a checkbox</label>
-			</div>
-			<div className={`h4 bg-${this.state.theme} text-white text-center p-2`}>
+		return <div className="m-2" onClick={this.handleClick}>
+			<div className={`h4 bg-${this.state.theme}
+						text-white text-center p-2`}>
 				{this.state.message}
 			</div>
-			<div className="text-center">
-				<button className="btn btn-primary"
-					onClick={(e) => this.handleEvent(e, "primary")}>Normal
-				</button>
-				<button className="btn btn-danger m-1"
-					onClick={(e) => this.handleEvent(e, "danger")}>Danger
-				</button>
+			<div className="text-center" onClick={this.handleClick}>
+				<ThemeButton theme="primary" callback={this.selectTheme} />
+				<ThemeButton theme="danger" callback={this.selectTheme} />
 			</div>
 		</div>
 	}
