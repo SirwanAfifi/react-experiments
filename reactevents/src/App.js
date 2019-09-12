203ab0f3a1af65703ab8a77fe4cf6d8dacfde09e
sirwan <sir1afifi@gmail.com>
2019-09-12 10:53:51 +0430

    default action

--- reactevents/src/App.js
+++ reactevents/src/App.js
@@ -19,8 +19,20 @@ export default class App extends Component {
 			() => this.setState({ message: `${event.type}: ${this.state.counter}` }));
 	}
 
+	toggleCheckbox = (event) => {
+		if (this.state.counter === 0) {
+			event.preventDefault();
+		}
+	}
+
 	render = () => {
 		return <div className="m-2">
+			<div className="form-check">
+				<input className="form-check-input"
+					type="checkbox"
+					onClick={this.toggleCheckbox} />
+				<label>This is a checkbox</label>
+			</div>
 			<div className={`h4 bg-${this.state.theme} text-white text-center p-2`}>
 				{this.state.message}
 			</div>
