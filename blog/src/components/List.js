7f48921f4ad46f030155f507c862938d6f157698
sirwan <sir1afifi@gmail.com>
2019-09-15 01:38:28 +0430

    show the update process

--- blog/src/components/List.js
+++ blog/src/components/List.js
@@ -2,40 +2,38 @@ import React, { Component } from 'react';
 import { Post } from './Post';
 import { ActionButton } from './ActionButton';
 export class List extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            posts: [
-                { title: "Post1", body: "Hello" },
-                { title: "Post2", body: "Hello" },
-                { title: "Post3", body: "Hello" },
-                { title: "Post4", body: "Hello" }
-            ],
-            showSpan: false
-        }
-    }
+	constructor(props) {
+		super(props);
+		this.state = {
+			posts: [
+				{ title: "Post1", body: "Hello" },
+				{ title: "Post2", body: "Hello" },
+				{ title: "Post3", body: "Hello" },
+				{ title: "Post4", body: "Hello" }
+			],
+			showDiv: false
+		}
+	}
 
-    sort = () => {
-        this.setState({ posts: this.state.posts.reverse(), showSpan: true })
-    }
+	sort = () => {
+		this.setState({ posts: this.state.posts.reverse(), showDiv: true })
+	}
 
-    getMessageElement() {
-        let div = <div id="boxDiv" className="box">
-            Hello
-        </div>
-        return this.state.showSpan ? <>{div}</> : div;
-    }
+	getListElement() {
+		const div = <div id="boxDiv" className="box">
+			{this.state.posts.map((post, index) => {
+				return <Post post={post} key={index} />
+			})}
+		</div>;
 
-    render() {
-        return <>
-            <h1 className="title">Latest posts</h1>
-            <div className="box">
-                <ActionButton text="Sort" callback={this.sort} />
-            </div>
-            {this.getMessageElement()}
-            {this.state.posts.map((post, index) => {
-                return <Post post={post} key={index} />
-            })}
-        </>
-    }
+		return this.state.showDiv ? <div>{div}</div> : div;
+	}
+
+	render() {
+		return <>
+			<h1 className="title">Latest posts</h1>
+			<div className="box"><ActionButton text="Sort" callback={this.sort} /></div>
+			{this.getListElement()}
+		</>
+	}
 }
