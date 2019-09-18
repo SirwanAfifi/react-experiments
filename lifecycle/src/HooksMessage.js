3b377762e2276bd88914b674e921b3342bdc6aaf
sirwan <sir1afifi@gmail.com>
2019-09-18 10:52:24 +0430

    use cleanup function in useEffect

--- lifecycle/src/HooksMessage.js
+++ lifecycle/src/HooksMessage.js
@@ -2,27 +2,30 @@ import React, { useState, useEffect } from 'react';
 import { ActionButton } from "./ActionButton";
 
 export function HooksMessage(props) {
-    const [showSpan, setSpan] = useState(false);
+	const [showSpan, setSpan] = useState(false);
 
-    useEffect(() => console.log("useEffect function invoked"));
+	useEffect(() => {
+		console.log("useEffect function invoked")
+		return () => console.log("useEffect cleanup");
+	});
 
-    const handleClick = (event) => {
-        setSpan(!showSpan);
-        props.callback(event);
-    }
+	const handleClick = (event) => {
+		setSpan(!showSpan);
+		props.callback(event);
+	}
 
-    const getMessageElement = () => {
-        let div = <div id="messageDiv" className="h5 text-center p-2">
-            {props.message}
-        </div>;
+	const getMessageElement = () => {
+		let div = <div id="messageDiv" className="h5 text-center p-2">
+			{props.message}
+		</div>;
 
-        return showSpan ? <span>{div}</span> : div;
-    }
+		return showSpan ? <span>{div}</span> : div;
+	}
 
-    return (
-        <div>
-            <ActionButton theme="primary" {...props} callback={handleClick} />
-            {getMessageElement()}
-        </div>
-    )
+	return (
+		<div>
+			<ActionButton theme="primary" {...props} callback={handleClick} />
+			{getMessageElement()}
+		</div>
+	)
 }
