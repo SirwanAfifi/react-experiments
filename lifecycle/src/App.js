05939d2889b6406f268c578351d3449d96a62d01
sirwan <sir1afifi@gmail.com>
2019-09-18 10:21:12 +0430

    create new instance

--- lifecycle/src/App.js
+++ lifecycle/src/App.js
@@ -6,7 +6,8 @@ export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      counter: 0
+      counter: 0,
+      showMessage: true
     };
   }
 
@@ -14,16 +15,26 @@ export default class App extends Component {
     this.setState({ counter: this.state.counter + 1 });
   }
 
+  handleChange = () => {
+    this.setState({ showMessage: !this.state.showMessage });
+  }
+
   render() {
     console.log("Render App Component");
     return <div className="container text-center">
       <div className="row p-2">
-        <div className="col-6">
-          <Message message={`Counter: ${this.state.counter}`}
+        <div className="col-4">
+          <div className="form-check">
+            <input type="checkbox" className="form-check-input"
+              checked={this.state.showMessage}
+              onChange={this.handleChange} />
+            <label className="form-check-label">Show</label>
+          </div>
+          {this.state.showMessage && <Message message={`Counter: ${this.state.counter}`}
             callback={this.incrementCounter}
-            text="Increment Counter" />
+            text="Increment Counter" />}
         </div>
-        <div className="col-6">
+        <div className="col-4">
           <List />
         </div>
       </div>
