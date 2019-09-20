635b750939d0eb9072045d617730e3faa45b8b9d
sirwan <sir1afifi@gmail.com>
2019-09-20 14:01:24 +0430

    Add reverse functionality

--- composition/src/ThemeSelector.js
+++ composition/src/ThemeSelector.js
@@ -3,7 +3,8 @@ export class ThemeSelector extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			theme: "primary"
+			theme: "primary",
+			reverseChildren: false
 		}
 		this.themes = ["primary", "secondary", "success", "warning", "dark"];
 	}
@@ -12,12 +13,22 @@ export class ThemeSelector extends Component {
 		this.setState({ theme: event.target.value });
 	}
 
+	toggleReverse = () => {
+		this.setState({ reverseChildren: !this.state.reverseChildren });
+	}
+
 	render() {
 		let modChildren = React.Children.map(this.props.children,
 			(c => React.cloneElement(c, { theme: this.state.theme })));
 
+		if (this.state.reverseChildren) {
+			modChildren.reverse();
+		}
 		return (
 			<div className="bg-dark p-2">
+				<button className="btn btn-primary" onClick={this.toggleReverse}>
+					Reverse
+				</button>
 				<div className="form-group text-left">
 					<label className="text-white">Theme: </label>
 					<select className="form-control" value={this.state.theme}
