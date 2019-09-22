032c691fa8040dfe3ebab2231b13b8dba877e56e
sirwan <sir1afifi@gmail.com>
2019-09-22 14:04:09 +0330

    Add procontroller

--- composition/src/App.js
+++ composition/src/App.js
@@ -4,15 +4,22 @@ import React, { Component } from 'react';
 //import { ThemeSelector } from './ThemeSelector';
 import { GeneralList } from './GeneralList';
 import { SortedList } from "./SortedList";
+import { ProFeature } from "./ProFeature";
+const ProList = ProFeature(SortedList);
 export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       // counter: 0
-      names: ["Zoe", "Bob", "Alice", "Dora", "Joe"]
+      names: ["Zoe", "Bob", "Alice", "Dora", "Joe"],
+      cities: ["London", "New York", "Paris", "Milan", "Boston"],
+      proMode: false
     };
   }
 
+  toggleProMode = () => {
+    this.setState({ proMode: !this.state.proMode });
+  }
   // incrementCounter = () => {
   //   this.setState({ counter: this.state.counter + 1 });
   // }
@@ -21,11 +28,29 @@ export default class App extends Component {
     return (
       <div className="container-fluid">
         <div className="row">
-          <div className="col-6">
+          <div className="col-12 text-center p-2">
+            <div className="form-check">
+              <input type="checkbox" className="form-check-input"
+                value={this.state.proMode}
+                onChange={this.toggleProMode} />
+              <label className="form-check-label">Pro Mode</label>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-3">
             <GeneralList list={this.state.names} theme="primary" />
           </div>
-          <div className="col-6">
-            <SortedList list={this.state.names} />
+          <div className="col-3">
+            <ProList list={this.state.names}
+              pro={this.state.proMode} />
+          </div>
+          <div className="col-3">
+            <GeneralList list={this.state.cities} theme="secondary" />
+          </div>
+          <div className="col-3">
+            <ProList list={this.state.cities}
+              pro={this.state.proMode} />
           </div>
         </div>
       </div>
--- composition/src/ProFeature.js
+++ composition/src/ProFeature.js
@@ -1,13 +1,15 @@
 import React from 'react';
 export function ProFeature(FeatureComponent) {
     return function (props) {
-        if (props.pro) {
-            const { pro, ...childProps } = props;
+        if (props.feature) {
+            let { pro, ...childProps } = props;
             return <FeatureComponent {...childProps} />
         } else {
-            <h5 className="bg-warning text-white text-center">
-                This is a Pro Feature
-            </h5>
+            return (
+                <h5 className="bg-warning text-white text-center">
+                    This is a Pro Feature
+                </h5>
+            )
         }
     }
 }
