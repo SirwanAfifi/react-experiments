2da6f6448f790545bf67595b3bb4749dc61b18d7
sirwan <sir1afifi@gmail.com>
2019-09-23 09:29:35 +0330

    Change action button component

--- composition/src/ActionButton.js
+++ composition/src/ActionButton.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 export class ActionButton extends Component {
-    render() {
-        return (
-            <button className={`btn btn-${this.props.theme} m-2`}
-                onClick={this.props.callback}>
-                {this.props.text}
-            </button>
-        );
-    }
+	render() {
+		return (
+			<button className={this.getClasses(this.props.proMode)}
+				disabled={!this.props.propMode}
+				onClick={this.props.callback}>
+				{this.props.text}
+			</button>
+		);
+	}
+
+	getClasses(proMode) {
+		let col = proMode ? this.props.theme : "danger";
+		return `btn btn-${col} m-2`;
+	}
 }
