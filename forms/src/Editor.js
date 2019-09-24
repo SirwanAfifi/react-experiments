0c1a0ba5d3c0081cbaf219679a20c53060bf0011
sirwan <sir1afifi@gmail.com>
2019-09-24 18:34:54 +0330

    Added editor component

---
+++ forms/src/Editor.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export class Editor extends Component {
+    render() {
+        return (
+            <div className="h5 bg-info text-white p-2">
+                Form Will Go Here
+             </div>
+        );
+    }
+}
