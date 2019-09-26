eb9b5a1963af2c31ac792eeca6c1565fe6570b9a
sirwan <sir1afifi@gmail.com>
2019-09-26 09:31:56 +0330

    Add select inputs

--- forms/src/Editor.js
+++ forms/src/Editor.js
@@ -5,12 +5,27 @@ export class Editor extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            name: ""
+            name: "",
+            flavor: "Vanilla",
+            toppings: ["Strawberries"]
         };
+
+        this.flavors = ["Chocolate", "Double Chocolate",
+            "Triple Chocolate", "Vanilla"];
+        this.toppings = ["Sprinkles", "Fudge Sauce",
+            "Strawberries", "Maple Syrup"]
     }
 
     updateFormValue = (event) => {
         this.setState({ [event.target.name]: event.target.value }, () => this.props.submit(this.state))
+    }
+
+    updateFormValueOptions = (event) => {
+        let options = [...event.target.options]
+            .filter(o => o.selected).map(o => o.value);
+        this.setState({ [event.target.name]: options }, () =>
+            this.props.submit(this.state));
+
     }
 
     render() {
@@ -22,6 +37,26 @@ export class Editor extends Component {
                         name="name"
                         value={this.state.name}
                         onChange={this.updateFormValue} />
+                </div>
+
+                <div className="form-group">
+                    <label>Ice Cream Flavors</label>
+                    <select className="form-control"
+                        name="flavor" value={this.state.flavor} onChange={this.updateFormValue}>
+                        {this.flavors.map(flavor =>
+                            <option value={flavor} key={flavor}>{flavor}</option>)}
+                    </select>
+                </div>
+
+                <div className="form-group">
+                    <label>Ice Cream Toppings</label>
+                    <select name="toppings" className="form-control" multiple={true} value={this.state.toppings} onChange={this.updateFormValueOptions}
+                    >
+                        {this.toppings.map(top =>
+                            <option key={top} value={top}>
+                                {top}
+                            </option>)}
+                    </select>
                 </div>
             </div>
         );
