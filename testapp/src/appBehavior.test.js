d2b40200cab9d613b307ed7e89ad19845b565c1f
sirwan <sir1afifi@gmail.com>
2019-10-01 09:29:45 +0330

    Testing effects of methods

--- testapp/src/appBehavior.test.js
+++ testapp/src/appBehavior.test.js
@@ -6,12 +6,24 @@ import App from "./App";
 Enzyme.configure({ adapter: new Adapter() });
 
 it("use title prop", () => {
-    const titleVal = "test title";
-    const wrapper = shallow(<App title={titleVal} />)
+	const titleVal = "test title";
+	const wrapper = shallow(<App title={titleVal} />)
 
-    const firstTitle = wrapper.find("h5").text();
-    const stateValue = wrapper.state("title");
+	const firstTitle = wrapper.find("h5").text();
+	const stateValue = wrapper.state("title");
 
-    expect(firstTitle).toBe(titleVal);
-    expect(stateValue).toBe(titleVal);
+	expect(firstTitle).toBe(titleVal);
+	expect(stateValue).toBe(titleVal);
+});
+
+it("updates state data", () => {
+	const wrapper = shallow(<App />);
+	const values = [10, 20, 30];
+
+	values.forEach((val, index) =>
+		wrapper.instance().updateFieldValue(index + 1, val));
+	wrapper.instance().updateTotal();
+
+	expect(wrapper.state("total"))
+		.toBe(values.reduce((total, val) => total + val), 0);
 });
