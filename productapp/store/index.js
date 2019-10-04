63f8de4461d7f7b9310e5679155fbf0be036d13f
sirwan <sir1afifi@gmail.com>
2019-10-04 10:46:14 +0330

    Add redux building blocks

---
+++ productapp/store/index.js
@@ -0,0 +1,5 @@
+import { createStore } from "redux";
+import modelReducer from "./modelReducer";
+export default createStore(modelReducer);
+export { saveProduct, saveSupplier, deleteProduct, deleteSupplier }
+    from "./modelActionCreators";
---
+++ productapp/store/modelActionTypes.js
@@ -0,0 +1,4 @@
+// Three events
+export const STORE = "STORE";
+export const UPDATE = "UPDATE";
+export const DELETE = "DELETE";
