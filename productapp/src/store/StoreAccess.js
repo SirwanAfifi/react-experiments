81b47322bdbe93915e0720758ff7f0a3fe212388
sirwan <sir1afifi@gmail.com>
2019-10-06 11:39:32 +0330

    Update store

--- productapp/src/store/StoreAccess.js
+++ productapp/src/store/StoreAccess.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { startCreatingProduct } from "./stateActions";
 export class StoreAccess extends Component {
     constructor(props) {
         super(props);
@@ -6,18 +7,47 @@ export class StoreAccess extends Component {
             product: (storeState) => storeState.modelData.products[0],
             state: (storeState) => storeState.stateData
         }
+        this.state = this.selectData();
     }
     render() {
-        return <div className="bg-info">
-            <pre className="text-white">
-                {JSON.stringify(this.props.store.getState(), null, 2)}
-            </pre>
-        </div>
+        return <React.Fragment>
+            <div className="text-center">
+                <button className="btn btn-primary m-1"
+                    onClick={this.dispatchAction}>
+                    Dispatch Action
+                </button>
+            </div>
+            <div className="bg-info">
+                <pre className="text-white">
+                    {JSON.stringify(this.state, null, 2)}
+                </pre>
+            </div>
+        </React.Fragment>
+    }
+
+    dispatchAction = () => {
+        this.props.store.dispatch(startCreatingProduct())
     }
 
     selectData() {
         let storeState = this.props.store.getState();
         return Object.entries(this.selectors).map(([k, v]) => [k, v(storeState)])
             .reduce((result, [k, v]) => ({ ...result, [k]: v }), {});
+    }
+
+    handleDataStoreChange() {
+        let newData = this.selectData();
+        Object.keys(this.selectors)
+            .filter(key => this.state[key] !== newData[key])
+            .forEach(key => this.setState({ [key]: newData[key] }));
+    }
+
+    componentDidMount() {
+        this.unsubscriber =
+            this.props.store.subscribe(() => this.handleDataStoreChange());
+    }
+
+    componentWillUnmount() {
+        this.unsubscriber();
     }
 }
