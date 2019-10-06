f023f907154bda70af7de934f66d048512de4771
sirwan <sir1afifi@gmail.com>
2019-10-06 11:11:19 +0330

    Update store access

--- productapp/src/store/StoreAccess.js
+++ productapp/src/store/StoreAccess.js
@@ -1,10 +1,23 @@
 import React, { Component } from "react";
 export class StoreAccess extends Component {
+    constructor(props) {
+        super(props);
+        this.selectors = {
+            product: (storeState) => storeState.modelData.products[0],
+            state: (storeState) => storeState.stateData
+        }
+    }
     render() {
         return <div className="bg-info">
             <pre className="text-white">
                 {JSON.stringify(this.props.store.getState(), null, 2)}
             </pre>
         </div>
+    }
+
+    selectData() {
+        let storeState = this.props.store.getState();
+        return Object.entries(this.selectors).map(([k, v]) => [k, v(storeState)])
+            .reduce((result, [k, v]) => ({ ...result, [k]: v }), {});
     }
 }
