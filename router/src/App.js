48d16f21fedf29430ffe19fa19ddfa56065769d3
sirwan <sir1afifi@gmail.com>
2019-10-06 12:07:12 +0330

    Add react router

--- router/src/App.js
+++ router/src/App.js
@@ -1,9 +1,26 @@
 import React, { Component } from 'react';
-
+import { BrowserRouter as Router, Link, Route } from "react-router-dom";
 export default class App extends Component {
     render() {
         return (
-            <h1>Hello</h1>
+            <Router>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-2">
+                            <div><Link to="/products">Products</Link></div>
+                            <div><Link to="/suppliers">Suppliers</Link></div>
+                        </div>
+                        <div className="col">
+                            <Route path="/products" render={() => {
+                                return <h1>Products</h1>
+                            }} />
+                            <Route path="/suppliers" render={() => {
+                                return <h1>Suppliers</h1>
+                            }} />
+                        </div>
+                    </div>
+                </div>
+            </Router>
         );
     }
 }
