843578f8f8053baa4d8438f1887f3e43851f8b5f
sirwan <sir1afifi@gmail.com>
2019-10-06 11:05:36 +0330

    Add new component to App

--- productapp/src/App.js
+++ productapp/src/App.js
@@ -5,17 +5,24 @@ import dataStore from "./store";
 import { Selector } from "./Selector";
 import { ProductDisplay } from "./ProductDisplay";
 import { SupplierDisplay } from "./SupplierDisplay";
-
+import { StoreAccess } from "./store/StoreAccess";
 export default class App extends Component {
 
     render() {
-        return (
-            <Provider store={ dataStore }>
-                <Selector>
-                    <ProductDisplay name="Products" />
-                    <SupplierDisplay name="Suppliers" />
-                </Selector>      
-            </Provider>
-        )
+        return <div className="container-fluid">
+            <div className="row">
+                <div className="col-3">
+                    <StoreAccess store={dataStore} />
+                </div>
+                <div className="col">
+                    <Provider store={dataStore}>
+                        <Selector>
+                            <ProductDisplay name="Products" />
+                            <SupplierDisplay name="Suppliers" />
+                        </Selector>
+                    </Provider>
+                </div>
+            </div>
+        </div>
     }
 }
