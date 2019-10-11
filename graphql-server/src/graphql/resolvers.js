2cb747d58e57a248d9fef8343028ec34d041ee05
sirwan <sir1afifi@gmail.com>
2019-10-11 10:51:35 +0330

    Add mutations

--- graphql-server/src/graphql/resolvers.js
+++ graphql-server/src/graphql/resolvers.js
@@ -1,4 +1,11 @@
 var data = require("../../restData")();
+
+const mapIdsToProducts = (supplier, nameFilter) =>
+	supplier.products.map(id => data.products.find(p => p.id === Number(id)))
+		.filter(p => p.name.toLowerCase().includes(nameFilter.toLowerCase()));
+
+
+let nextId = 100;
 module.exports = {
 	products: () => data.products,
 	product: (args) => data.products.find(p => p.id === parseInt(args.id)),
@@ -13,6 +20,34 @@ module.exports = {
 				...result,
 				products: () => result.products.map(id =>
 					data.products.find(p => p.id === Number(id)))
+			}
+		}
+	},
+
+	storeProduct({ product }) {
+		if (product.id == null) {
+			product.id = nextId++;
+			data.products.push(product);
+		} else {
+			product = { ...product, id: Number(product.id) };
+			data.products = data.products
+				.map(p => p.id === product.id ? product : p);
+		}
+		return product;
+	},
+	storeSupplier(args) {
+		const supp = { ...args, id: Number(args.id) };
+		if (args.id == null) {
+			supp.id = nextId++;
+			data.suppliers.push(supp)
+		} else {
+			data.suppliers = data.suppliers.map(s => s.id === supp.id ? supp : s);
+		}
+		let result = data.suppliers.find(s => s.id === supp.id);
+		if (result) {
+			return {
+				...result,
+				products: ({ nameFilter }) => mapIdsToProducts(result, nameFilter)
 			}
 		}
 	}
