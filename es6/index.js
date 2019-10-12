3ee48d92fa24245403685e2d05cd45e7e3596429
sirwan <sir1afifi@gmail.com>
2019-10-12 23:02:08 +0330

    use Arrow in outter function

--- es6/index.js
+++ es6/index.js
@@ -1,10 +1,10 @@
 var tahoe = {
     resorts: ["Kirkwood", "Squaw", "Alpine", "Heavenly", "Northstar"],
-    print: function (delay = 1000) {
+    print: (delay = 1000) => {
         setTimeout(() => {
             console.log(this.resorts.join(", "))
         }, delay);
     }
 };
 
-tahoe.print() // Cannot read property 'join' of undefined
+tahoe.print()
