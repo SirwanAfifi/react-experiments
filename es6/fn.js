436b38338ccdf473a6dfc607e81988aa7b9aa550
sirwan <sir1afifi@gmail.com>
2019-10-13 23:42:43 +0330

    add fn to array

--- es6/fn.js
+++ es6/fn.js
@@ -4,11 +4,20 @@
 
 // var log = message => console.log(message);
 
-const obj = {
-    message: "They can be added to objects like variables",
-    log(message) {
-        console.log(message)
-    }
-};
+// const obj = {
+//     message: "They can be added to objects like variables",
+//     log(message) {
+//         console.log(message)
+//     }
+// };
 
-obj.log(obj.message)
+// obj.log(obj.message)
+
+const messages = [
+    "They can be inserted into arrays",
+    message => console.log(message),
+    "like variables",
+    message => console.log(message)
+];
+
+messages[1](messages[0]) // They can be inserted into arrays messages[3](messages[2]) // like variables
