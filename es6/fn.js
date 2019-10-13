1388c49478f788a66bd5d9d8720ca2ca744fc635
sirwan <sir1afifi@gmail.com>
2019-10-13 23:53:53 +0330

    imperative approach

--- es6/fn.js
+++ es6/fn.js
@@ -22,4 +22,18 @@
 
 // messages[1](messages[0]) // They can be inserted into arrays messages[3](messages[2]) // like variables
 
-const createScream = logger => message => logger(message.toUpperCase() + "!!!");
+// const createScream = logger => message => logger(message.toUpperCase() + "!!!");
+
+var string = "This is the midday show with Cheryl Waters";
+var urlFriendly = "";
+
+for (var i = 0; i < string.length; i++) {
+	if (string[i] === " ") {
+		urlFriendly += "-";
+	} else {
+		urlFriendly += string[i];
+	}
+}
+
+console.log(string);
+console.log(urlFriendly);
