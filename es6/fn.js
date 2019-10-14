4f60c6ab7fd04e1258b78005a11bbee69384ed55
sirwan <sir1afifi@gmail.com>
2019-10-14 23:36:49 +0330

    change id to hashs

--- es6/fn.js
+++ es6/fn.js
@@ -1,20 +1,32 @@
-const ages = [21, 18, 42, 40, 64, 63, 34];
-
-const maxAge = ages.reduce((max, age) => {
-	console.log(`${age} > ${max} = ${age > max}`);
-	if (age > max) {
-		return age
-	} else {
-		return max
+const colors = [
+	{
+		id: '-xekare',
+		title: "rad red",
+		rating: 3
+	},
+	{
+		id: '-jbwsof',
+		title: "big blue",
+		rating: 2
+	},
+	{
+		id: '-prigbj',
+		title: "grizzly grey",
+		rating: 5
+	},
+	{
+		id: '-ryhbhsl',
+		title: "banana",
+		rating: 1
 	}
-}, 0);
-console.log('maxAge', maxAge);
+];
 
-// 21 > 0 = true
-// 18 > 21 = false
-// 42 > 21 = true
-// 40 > 42 = false
-// 64 > 42 = true
-// 63 > 64 = false
-// 34 > 64 = false
-// maxAge 64
+const hashColors = colors.reduce(
+	(hash, { id, title, rating }) => {
+		hash[id] = { title, rating }
+		return hash
+	},
+	{}
+);
+
+console.log(hashColors)
