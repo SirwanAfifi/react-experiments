258dd23d88a3f1a2cd87757551bf5913d445a3cc
sirwan <sir1afifi@gmail.com>
2019-10-23 18:15:47 +0330

    Add inverse data flow

--- color-organiser/src/App.js
+++ color-organiser/src/App.js
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { AddColorForm } from './AddColorForm';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+
+  logColor = (title, color) =>
+    console.log(`New color: ${title} | ${color}`)
+
+  render() {
+    return (
+      <AddColorForm onNewColor={this.logColor} />
+    );
+  }
 }
-
-export default App;
