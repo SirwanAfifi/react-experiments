52785333e94fd4369f49e7cf935a3262b27b1611
sirwan <sir1afifi@gmail.com>
2019-10-23 18:20:26 +0330

    Ref in stateless component

--- color-organiser/src/AddColorForm.js
+++ color-organiser/src/AddColorForm.js
@@ -1,30 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-export class AddColorForm extends Component {
-    constructor(props) {
-        super(props);
-        this.submit = this.submit.bind(this);
-    }
-
-    submit(e) {
-        const { _title, _color } = this.refs
-        e.preventDefault();
-        this.props.onNewColor(_title.value, _color.value);
-        _title.value = '';
-        _color.value = '#000000';
-        _title.focus();
-    }
-
-    render() {
-        return (
-            <form onSubmit={this.submit}>
-                <input ref="_title"
-                    type="text"
-                    placeholder="color title..." required />
-                <input ref="_color"
-                    type="color" required />
-                <button>Add</button>
-            </form>
-        );
-    }
+export const AddColorForm = ({ onNewColor = f => f }) => {
+	let _title, _color
+	const submit = e => {
+		e.preventDefault();
+		onNewColor(_title.value, _color.value);
+		_title.value = '';
+		_color.value = '#000000';
+		_title.focus();
+	}
+	return (
+		<form onSubmit={submit}>
+			<input ref={input => _title = input}
+				type="text"
+				placeholder="color title..." required />
+			<input ref={input => _color = input}
+				type="color" required />
+			<button>ADD</button>
+		</form>
+	)
 }
