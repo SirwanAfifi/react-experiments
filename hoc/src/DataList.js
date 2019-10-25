d6ac546cb2c4ec781dbae1d6f265961b2c8bc462
sirwan <sir1afifi@gmail.com>
2019-10-25 14:00:00 +0100

    Use componentDidMount

--- hoc/src/DataList.js
+++ hoc/src/DataList.js
@@ -1,30 +1,30 @@
 const DataComponent = (ComposedComponent, url) =>
-    class DataComponent extends Component {
-        constructor(props) {
-            super(props);
-            this.state = {
-                data: [],
-                loading: false,
-                loaded: false
-            }
-        }
-        componentWillMount() {
-            this.setState({ loading: true });
-            fetch(url)
-                .then(response => response.json())
-                .then(data => this.setState({
-                    loaded: true, loading: false, data
-                }))
-        }
+	class DataComponent extends Component {
+		constructor(props) {
+			super(props);
+			this.state = {
+				data: [],
+				loading: false,
+				loaded: false
+			}
+		}
+		componentDidMount() {
+			this.setState({ loading: true });
+			fetch(url)
+				.then(response => response.json())
+				.then(data => this.setState({
+					loaded: true, loading: false, data
+				}))
+		}
 
-        render() {
-            return (
-                <div className="data-component">
-                    {(this.state.loading) ?
-                        <div>Loading...</div> :
-                        <ComposedComponent {...this.state} />
-                    }
-                </div>
-            );
-        }
-    }
+		render() {
+			return (
+				<div className="data-component">
+					{(this.state.loading) ?
+						<div>Loading...</div> :
+						<ComposedComponent {...this.state} />
+					}
+				</div>
+			);
+		}
+	}
