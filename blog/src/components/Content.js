c95e351310bf48a86c017b9ae940babca225afd0
sirwan <sir1afifi@gmail.com>
2019-10-30 22:44:42 +0330

    Remove map

--- blog/src/components/Content.js
+++ blog/src/components/Content.js
@@ -2,10 +2,6 @@ import React, { Component } from 'react';
 
 export class Content extends Component {
     render() {
-        React.Children.map(child => {
-            console.log(child)
-        })
-
         return (
             <div className="content">
                 {this.props.children}
