4679e42ca2eb4dcbd523c52c6f77ab2d73ec7a00
sirwan <sir1afifi@gmail.com>
2019-10-31 11:09:06 +0330

    Add tooltip

--- tooltip/src/App.js
+++ tooltip/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Tooltip } from './Tooltip';
 
 function App() {
   return (
@@ -8,16 +9,19 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <Tooltip text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."><code>src/App.js</code></Tooltip> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+
+        <Tooltip text="Learn React">
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
         </a>
+        </Tooltip>
       </header>
     </div>
   );
---
+++ tooltip/src/Tooltip.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import ReactDOM from "react-dom";
+export class Tooltip extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = { opacity: false }
+        this.toggle = this.toggle.bind(this)
+    }
+    toggle() {
+        const tooltipNode = ReactDOM.findDOMNode(this)
+        this.setState({
+            opacity: !this.state.opacity,
+            top: tooltipNode.offsetTop,
+            left: tooltipNode.offsetLeft
+        })
+    }
+    render() {
+        const style = {
+            zIndex: (this.state.opacity) ? 1000 : -1000,
+            opacity: +this.state.opacity,
+            top: (this.state.top || 0) + 45,
+            left: (this.state.left || 0) + 30
+        }
+        return (
+            <div style={{ display: 'inline' }}>
+                <span style={{ color: 'blue' }} onMouseEnter={this.toggle} onMouseOut={this.toggle}>
+                    {this.props.children}
+                </span>
+                <div className="tooltip bottom" style={style} role="tooltip">
+                    <div className="tooltip-arrow"></div>
+                    <div className="tooltip-inner">
+                        {this.props.text}
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
