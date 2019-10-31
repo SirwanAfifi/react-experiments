113d03f7618bb16cc6ff1259bcf5deef52c95ea5
sirwan <sir1afifi@gmail.com>
2019-10-31 10:28:50 +0330

    Add HOC

---
+++ hoc/src/LoadWebsite.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const LoadWebstie = (Component) => {
+	class _LoadWebsite extends React.Component {
+		constructor(props) {
+			super(props);
+			this.state = { label: "Run" };
+			this.state.handleClick = this.handleClick.bind(this);
+		}
+
+		getUrl() {
+			return "https://facebook.github.io/react/docs/top-level-api.html";
+		}
+
+		handleClick(event) {
+			var iframe = document.getElementById("frame").src =
+				this.getUrl();
+		}
+
+		componentDidMount() {
+			console.log(ReactDOM.findDOMNode(this));
+		}
+
+		render() {
+			console.log(this.state);
+			return <Component {...this.state} {...this.props} />
+		}
+	}
+
+	_LoadWebsite.displayName = "EnhancedComponent";
+	return _LoadWebsite;
+}
