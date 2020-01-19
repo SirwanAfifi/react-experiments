a8b244adc11157008e058c2477cdee504ae95c1f
sirwan <sir1afifi@gmail.com>
2020-01-19 20:31:50 +0330

    Add tailwind

---
+++ redux101/src/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./containers/App";
+import "./css/style.css";
+ReactDOM.render(<App />, document.querySelector("#root"));
