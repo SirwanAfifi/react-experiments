e6ca01d1df73ced81fc58930f19a007a3ecd224f
sirwan <sir1afifi@gmail.com>
2020-02-02 18:51:15 +0330

    init

---
+++ nextwithtailwindcs/pages/_app.js
@@ -0,0 +1,3 @@
+import App from "next/app";
+import "../styles/styles.css";
+export default App;
---
+++ nextwithtailwindcs/pages/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Home = () => (
+  <div className="container mx-auto">
+    <h1 className="text-6xl text-purple-600">Hello</h1>
+  </div>
+);
+
+export default Home;
