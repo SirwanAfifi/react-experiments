3e8e8bf8f7e4a5220bda84973068b2efb8498949
sirwan <sir1afifi@gmail.com>
2020-03-19 14:00:00 +0100

    Add styles

--- styled-component/src/App.tsx
+++ styled-component/src/App.tsx
@@ -1,5 +1,24 @@
 import React from "react";
+import styled, { css } from "styled-components";
 
 export default () => {
-  return <h1>Using Styled Components</h1>;
+  return (
+    <App>
+      <p>Using Styled Components</p>
+    </App>
+  );
 };
+
+const Common = css`
+  text-align: center;
+`;
+
+const App = styled.h1`
+  color: red;
+  font-size: 2rem;
+  ${Common}
+  p: {
+    font-size: 1.5rem;
+    color: red;
+  }
+`;
---
+++ styled-component/src/styles/globalStyles.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+const Header = styled.div`
+  text-align: center;
+  background: #e6e6e6;
+`;
+
+export default Header;
