df6b5db45a97cb23b3b6a684cc45a6184ee4ea49
sirwan <sir1afifi@gmail.com>
2020-04-08 10:24:32 +0430

    Add stopwatch

--- JS-OOP/index.js
+++ JS-OOP/index.js
@@ -1,27 +1,36 @@
-function Circle(radius) {
-  let defaultLocation = { x: 0, y: 0 };
-  let computeOptimumLocation = function (factor) {
-    console.log("Compute Optimum Location");
+function Stopwatch() {
+  let startTime,
+    endTime,
+    running,
+    duration = 0;
+
+  this.start = function () {
+    if (running) throw new Error("Stopwatch had already started.");
+
+    running = true;
+
+    startTime = new Date();
+  };
+  this.stop = function () {
+    if (!running) throw new Error("Stopwatch is not started.");
+
+    running = false;
+
+    endTime = new Date();
+
+    const seconds = (endTime.getTime() - startTime.getTime()) / 1000;
+    duration += seconds;
+  };
+  this.reset = function () {
+    startTime = null;
+    endTime = null;
+    running = false;
+    duration = 0;
   };
 
-  this.radius = radius;
-  this.draw = function () {
-    computeOptimumLocation(0.1);
-
-    console.log("draw");
-  };
-
-  Object.defineProperty(this, "defaultLocation", {
+  Object.defineProperty(this, "duration", {
     get: function () {
-      return defaultLocation;
-    },
-    set: function (value) {
-      if (!value.x || !value.y) throw new Error("Invalid Location");
-
-      defaultLocation = value;
+      return duration;
     },
   });
 }
-
-const circle = new Circle(10);
-circle.draw();
