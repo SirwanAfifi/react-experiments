be77bc85caa5e9b443134221cde633ca0291e982
sirwan <sir1afifi@gmail.com>
2020-04-08 09:50:54 +0430

    Add getter & setter

--- JS-OOP/index.js
+++ JS-OOP/index.js
@@ -10,6 +10,17 @@ function Circle(radius) {
 
     console.log("draw");
   };
+
+  Object.defineProperty(this, "defaultLocation", {
+    get: function () {
+      return defaultLocation;
+    },
+    set: function (value) {
+      if (!value.x || !value.y) throw new Error("Invalid Location");
+
+      defaultLocation = value;
+    },
+  });
 }
 
 const circle = new Circle(10);
