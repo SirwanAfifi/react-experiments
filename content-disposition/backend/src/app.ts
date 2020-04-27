7bdd5d2af9abb130f4d8d9c42ec2b718aa9e81cc
sirwan <sir1afifi@gmail.com>
2020-04-27 13:51:56 +0430

    Works automatically

--- content-disposition/backend/src/app.ts
+++ content-disposition/backend/src/app.ts
@@ -1,8 +1,16 @@
 import express, { Request, Response, NextFunction } from "express";
+import path from "path";
+
 const app = express();
+app.use("/public", express.static(path.join(__dirname, "..", "public")));
 
 app.get("/", (req: Request, res: Response) => {
   res.send({ name: "Sirwan" });
+});
+
+app.get("/download", (req: Request, res: Response) => {
+  const file = `public/uploads/users.csv`;
+  res.download(file); // Set disposition and send it.
 });
 
 export default app;
